e5e751c1c7092499c0cdb623abd6a740da2d0a8f
Eskolnik <eskolnik2021@Yandex.ru>
2024-01-24 22:37:17 +0300

    add program in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[5] { "hey", "12", "well", "hello", "wor" };
+string[] arrey2 = new string[array1.Length];
+
+void FinalArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            arrey2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FinalArray(array1, arrey2);
+PrintArray(arrey2);
